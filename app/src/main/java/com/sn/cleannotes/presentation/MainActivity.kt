db175bf81fec7f5131f46f9134f56fca5ae6b563
Saeed Noshadi <saeednoshadi89@gmail.com>
2021-02-17 02:24:04 +0330

    theme

--- app/src/main/java/com/sn/cleannotes/presentation/MainActivity.kt
+++ app/src/main/java/com/sn/cleannotes/presentation/MainActivity.kt
@@ -14,6 +14,7 @@ import com.sn.cleannotes.databinding.ActivityMainBinding
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         val navController = findNavController(R.id.navContiner)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
